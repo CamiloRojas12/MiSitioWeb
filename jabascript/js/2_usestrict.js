616bb4d015227390bc693120d00d6636767738ce
CamiloRojas12 <benitocamelo0002@gmail.com>
2021-12-07 20:18:44 -0500

    add javas

--- jabascript/js/2_usestrict.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-var nombreaprendiz1 = "liliana";
-nombreaprendiz2 = "kevin" ;
-console.log (nombreaprendiz1);
-console.log (nombreaprendiz2);
--- jabascript/js/7_operadoresrelaciones.js
+++
@@ -1,7 +0,0 @@
-"user strict"
-
-console.log("mayor que.....>")
-console.log("menor que.....<")
-console.log("mayor o igual que......>=")
-console.log("igual que.................==")
-console.log()
