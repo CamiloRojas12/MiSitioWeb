5d8d78f30b64f0b49e224f4296a4a7742f6305b8
CamiloRojas12 <benitocamelo0002@gmail.com>
2021-12-17 15:12:26 -0500

    Subir arreglos

---
+++ javascript/js/12_mayuscula_minuscula.js
@@ -0,0 +1,17 @@
+
+var ciudad1= "pitalito";
+ciudad1=ciudad1.toUpperCase();
+console.log (ciudad1);
+
+ciudad1=ciudad1.toLowerCase();
+console.log(ciudad1)
+
+var nombre="Camilo Andres Rojas";
+nombre=nombre.toUpperCase();
+console.log(nombre);
+
+nombre=nombre.toLowerCase();
+console.log(nombre);
+
+
+
