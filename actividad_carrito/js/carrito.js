3b0319f687601a44bd170e6bf901b81913209889
CamiloRojas12 <benitocamelo0002@gmail.com>
2022-02-28 17:27:52 -0500

    Carrito

---
+++ actividad_carrito/js/carrito.js
@@ -0,0 +1,22 @@
+	let inicio = 0;
+	let cantidad = "";
+
+	function aumentar(){
+	    if( inicio <= 9) {
+	        cantidad = document.getElementById('cantidad').value = ++inicio;
+	        document.getElementById("total").innerHTML = cantidad;
+	    }
+	    calcularTotal();
+	}
+	 
+	function disminuir() {
+	    if(inicio >= 1) {
+	        cantidad = document.getElementById('cantidad').value = --inicio;
+	    }
+	}
+	function calcularTotal() {
+	    let valorUnidad = document.getElementById("valorUnitario").innerHTML;
+	    let cantidadElementos = document.getElementById("total").innerHTML;
+	    let valorCompra = valorUnidad * cantidadElementos;
+	    document.getElementById("multiplicar").innerHTML = valorCompra;
+	}
